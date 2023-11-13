fb2d030996762e9e2759d98802a0aa8bcb345566
Sunam Niroula <sunamniroula1@gmail.com>
2023-11-13 19:56:13 +0545

    Added Notebook of Exploratory Data Analysis(EDA) and Model Training and related

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -8,7 +8,7 @@ def error_message_detail(error,error_detail:sys):
      file_name,exc_tb.tb_lineno,str(error)
     )
     return error_message
-    
+
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
@@ -18,9 +18,12 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
+'''
+To check error messages
 if __name__ == "__main__":
     try:
         a = 1/0
     except Exception as e:
         logging.info("Divisible by Zero")
         raise CustomException(e, sys)
+'''
